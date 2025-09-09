3d4557e335255ed3c14fa718bc8594ea8009b01c
jamtuttlebee <jamtuttlebee@gmail.com>
2025-09-09 09:43:36 -0400

    Added a line for plotting the plot - and changed the color of the trend line from seashell to purple.

--- scripts/03_content/testscript.R
+++ scripts/03_content/testscript.R
@@ -7,10 +7,12 @@ data("data_lionfish")
 plot1 = ggplot(data = data_lionfish,
                mapping = aes(x = total_length_mm, y = depth_m)) +
   geom_point() +
-  geom_smooth(method='lm', colour="seashell4") +
+  geom_smooth(method='lm', colour="purple") +
   labs(title = "Depth Length Relationship") +
   xlab("Total Length (mm)") +
   ylab("Depth (m)") +
   theme_minimal()
 
 ggsave(plot1, path = "results/img/depth_vs_length.pdf")
+
+plot(plot1) # JT added line to plot
